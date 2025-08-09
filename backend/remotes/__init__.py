219a7a4e4a489d7ec11f39d87193432e17c51812
Pedro <ptsmonteiro@gmail.com>
2025-08-09 22:39:32 +0200

    Start from scratch

--- backend/remotes/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Remote modules for external QSO services."""
-
-# Expose individual service modules for convenience
-from . import clublog, lotw, ham365, qrz
-
-__all__ = ["clublog", "lotw", "ham365", "qrz"]
